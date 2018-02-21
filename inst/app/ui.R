2beaee694787c7a9ca763d71ff8e2f4cc1ffb417
Hanne Sigrun Byhring <hannesigrun@Hannes-MacBook.local>
2018-02-21 11:55:20 +0100

    kart og barplot lagt til

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -1,13 +1,35 @@
+library(shiny)
+library(leaflet)
+
 shinyUI(function(request){
   navbarPage(title = "",
-             tabPanel("input", sliderInput(inputId = "bins",
-                                           label = "Number of bins:",
-                                           min = 1,
-                                           max = 50,
-                                           value = 30)),
+#             tabPanel("input", sliderInput(inputId = "bins",
+#                                           label = "Number of bins:",
+#                                           min = 1,
+#                                           max = 50,
+#                                           value = 30)
+#                      ),
+             tabPanel("Velg tema", 
+                      sidebarPanel(selectInput(inputId = "kartlag",
+                                          label = "Velg et tema:",
+                                          choices = c("Personer til fastlege/legevakt", 
+                                                      "Personer på poliklinikk", 
+                                                      "Akuttinnlagte personer"))
+                      ),
+                      mainPanel(tableOutput("kolstabell")
+                        
+                      )
+             ),
+             
+             
+             
              navbarMenu(title="Plots",
-                        tabPanel("Waiting", plotOutput(outputId = "waiting")),
-                        tabPanel("Eruptions", plotOutput(outputId = "eruptions"))
+#                        tabPanel("Waiting", plotOutput(outputId = "waiting")),
+#                        tabPanel("Eruptions", plotOutput(outputId = "eruptions")),
+                         tabPanel("kart", leafletOutput("mymap")),
+                         tabPanel("Kols histogram", plotOutput(outputId = "kolshisto"))
              )
+
+            
   )
 })
