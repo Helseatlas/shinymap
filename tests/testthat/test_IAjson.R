836d49aeeada6f81d59acdcd34cffb65a18f9ff6
Arnfinn <arnfinn.steindal@gmail.com>
2018-09-07 09:07:14 +0200

    renamed test sets from *.js to *.json

--- tests/testthat/test_IAjson.R
+++ tests/testthat/test_IAjson.R
@@ -1,10 +1,10 @@
 context("Read IAjson")
 
 test_that("JSON files from IA can be read", {
-  file = "data/eldre.js"
+  file = "data/eldre.json"
   tmp <- readIAjson(json_file = file)
   expect_equal_to_reference(tmp, "data/json1.rds")
-  file = "data/eldre_eng.js"
+  file = "data/eldre_eng.json"
   tmp <- readIAjson(json_file = file)
   expect_equal_to_reference(tmp, "data/json2.rds")
 #  file = "data/barn.js"
