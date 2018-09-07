ff2ef31f3966483b4fa65567967b01d1e56dea4c
Arnfinn <arnfinn.steindal@uit.no>
2018-09-07 18:36:10 +0200

    Fixed bug in string replacement
    . is read as all letters

--- R/readJS.R
+++ R/readJS.R
@@ -23,9 +23,9 @@ readIAjson <- function(json_file = NULL, testing = FALSE){
   # Convert all special characters to "normal" characters if running tests,
   # because the sorting with special characters is system dependent.
   
-  conv_list1 <- list("æ", "ø", "å", "Æ",  "Ø", "Å", ".", "-")
-  conv_list2 <- list("ae","o", "a", "AE", "O", "å", "_", "_")
-  
+  conv_list1 <- list("æ", "ø", "å", "Æ",  "Ø", "Å", '-', "St. ")
+  conv_list2 <- list("ae","o", "a", "AE", "O", "å", "_", "St ")
+
   for (i in 1:length(conv_list1)){
 
     bo <- gsub(conv_list1[i], conv_list2[i], bo)
