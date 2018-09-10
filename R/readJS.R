8b31cf950a86af326e3c5bba5915ff4422dad734
Arnfinn <arnfinn.steindal@gmail.com>
2018-09-10 14:26:47 +0200

    Fixed character convertion so tests will work on win
    Used unicodes instead of Norwegian letters

--- R/readJS.R
+++ R/readJS.R
@@ -26,8 +26,8 @@ readIAjson <- function(json_file = NULL, testing = FALSE){
     # Convert all special characters to "normal" characters if running tests,
     # because the sorting with special characters is system dependent.
 
-    conv_list1 <- list("æ", "ø", "å", "Æ",  "Ø", "Å", '-', "St. ")
-    conv_list2 <- list("ae","o", "a", "AE", "O", "å", "", "St")
+    conv_list1 <- list("\u00E5", "\u00F8", "\u00E5", "\u00C6",  "\u00D8", "\u00C5", '-', "St. ")
+    conv_list2 <- list("ae","o", "aa", "AE", "O", "AA", "", "St")
 
     for (i in 1:length(conv_list1)){
       area <- gsub(conv_list1[i], conv_list2[i], area)
