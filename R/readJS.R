bf3dda538a8163add4be455f3d56fcfffffca77a
Arnfinn <arnfinn.steindal@gmail.com>
2018-09-10 14:11:10 +0200

    also rm spaces in character conv (for testing)

--- R/readJS.R
+++ R/readJS.R
@@ -8,41 +8,41 @@
 #' @importFrom magrittr "%>%"
 #'
 readIAjson <- function(json_file = NULL, testing = FALSE){
-  
+
   # Read the json file
   # NOTE: The js-file HAS to be converted from UTF-8 BOM to UTF-8 (in notepad++) before this will work!
   json_data <- jsonlite::fromJSON(json_file)
-  
+
   # make it a tibble data fram
   tbl <- tibble::as_data_frame(json_data$geographies)
-  
+
   # Names of areas are located in json_data$geographies$features
   area <- data.frame(tbl$features)$name
-  
+
   # Name of reference is located in json_data$geographies$comparisonFeatures
   ref_area <- data.frame(tbl$comparisonFeatures)$name
-  
+
   if (testing){
     # Convert all special characters to "normal" characters if running tests,
     # because the sorting with special characters is system dependent.
-    
+
     conv_list1 <- list("æ", "ø", "å", "Æ",  "Ø", "Å", '-', "St. ")
-    conv_list2 <- list("ae","o", "a", "AE", "O", "å", "_", "St ")
-    
+    conv_list2 <- list("ae","o", "a", "AE", "O", "å", "", "St")
+
     for (i in 1:length(conv_list1)){
       area <- gsub(conv_list1[i], conv_list2[i], area)
       ref_area <- gsub(conv_list1[i], conv_list2[i], ref_area)
     }
   }
-  
+
   # All data
   themes <- data.frame(tbl$themes) %>% tibble::as_data_frame()
-  
+
   # Test that number of highest level is equal the length of themes$indicators
   if (length(themes$name) != length(themes$indicators)){
     stop("Something fishy in your json file. ")
   }
-  
+
   # Define an empty data frame
   all_data <- data.frame()
   for (i in 1:length(themes$indicators)){
@@ -55,13 +55,13 @@ readIAjson <- function(json_file = NULL, testing = FALSE){
     rates <- data.frame(next_level$values)  %>% tibble::as_data_frame()
     # Rates for Norway etc
     ref_rates <- data.frame(next_level$comparisonValues)  %>% tibble::as_data_frame()
-    
-    # Extract the numeraters and denominators  
+
+    # Extract the numeraters and denominators
     extra <- data.frame(next_level$associates)
-    
+
     for (j in 1:length(next_level)){
       if (!is.na(next_level$id[j])){
-        
+
         k <- j - 1
         if (k == 0){
           post <- ""
@@ -74,7 +74,7 @@ readIAjson <- function(json_file = NULL, testing = FALSE){
         name_denominator <- extra[, paste0("name",post)][1]
         ref_numerater <-  data.frame(extra[, paste0("comparisonValues",post)][2])
         ref_denominator <- data.frame(extra[, paste0("comparisonValues",post)][1])
-        
+
         # Names for the second level
         level2 <- next_level$name[j]
         level3 <- NULL
@@ -83,28 +83,28 @@ readIAjson <- function(json_file = NULL, testing = FALSE){
         # ID for level 2 (not unique with three levels)
         selection_id <- next_level$id[j]
         if (is.null(level3)){
-          
+
           # Only for two-level atlases
-          combined <- data.frame(area, level1, level2, selection_id, rates[j], name_numerater, numerater, name_denominator, denominator, 0) 
+          combined <- data.frame(area, level1, level2, selection_id, rates[j], name_numerater, numerater, name_denominator, denominator, 0)
           colnames(combined) <- c("area", "level1", "level2", "id", "rate", "name_numerater", "numerater", "name_denominator", "denominator", "ref")
-          ref_combined <- data.frame(ref_area, level1, level2, selection_id, ref_rates[j], name_numerater, ref_numerater, name_denominator, ref_denominator, 1) 
+          ref_combined <- data.frame(ref_area, level1, level2, selection_id, ref_rates[j], name_numerater, ref_numerater, name_denominator, ref_denominator, 1)
           colnames(ref_combined) <- c("area", "level1", "level2", "id", "rate", "name_numerater", "numerater", "name_denominator", "denominator", "ref")
         } else { # Only for three level atlases
           if (level3 != prev_level3){ # If level3 is not equal to previous level3
             k = k + 1
             id2 <- paste0(selection_id, "j", k)
           }
-          combined <- data.frame(area, level1, level2, level3, id2, rates[j], name_numerater, numerater, name_denominator, denominator, 0) 
+          combined <- data.frame(area, level1, level2, level3, id2, rates[j], name_numerater, numerater, name_denominator, denominator, 0)
           colnames(combined) <- c("area", "level1", "level2", "level3", "id", "rate", "name_numerater", "numerater", "name_denominator", "denominator", "ref")
-          ref_combined <- data.frame(ref_area, level1, level2, level3, id2, ref_rates[j], name_numerater, ref_numerater, name_denominator, ref_denominator, 1) 
+          ref_combined <- data.frame(ref_area, level1, level2, level3, id2, ref_rates[j], name_numerater, ref_numerater, name_denominator, ref_denominator, 1)
           colnames(ref_combined) <- c("area", "level1", "level2", "level3", "id", "rate", "name_numerater", "numerater", "name_denominator", "denominator", "ref")
           prev_level3 <- level3
         }
         all_data <- rbind(all_data, combined, ref_combined)
       }
-    } 
+    }
   }
-  
+
   return(all_data)
-  
+
 }
