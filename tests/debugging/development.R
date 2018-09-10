935680ebcee189a8ffef900d4304aa606231e19c
Arnfinn <arnfinn.steindal@gmail.com>
2018-09-10 11:06:08 +0200

    rm me later...

--- tests/debugging/development.R
+++ tests/debugging/development.R
@@ -94,74 +94,95 @@ httr::set_config(httr::use_proxy(url="http://www-proxy.helsenord.no", port=8080)
 devtools::install_github("helseatlas/shinymap", ref = "readIAjson")
 
 
+json_file <- "tests/testthat/data/barn.json"
+
+
+# COPY OF readJS.R
+
 
 # Read the json file
+# NOTE: The js-file HAS to be converted from UTF-8 BOM to UTF-8 (in notepad++) before this will work!
 json_data <- jsonlite::fromJSON(json_file)
 
 # make it a tibble data fram
 tbl <- tibble::as_data_frame(json_data$geographies)
 
 # Names of areas are located in json_data$geographies$features
-bo <- data.frame(tbl$features)
+area <- data.frame(tbl$features)$name
 
 # Name of reference is located in json_data$geographies$comparisonFeatures
-ref <- data.frame(tbl$comparisonFeatures)
+ref_area <- data.frame(tbl$comparisonFeatures)$name
+
+testing <- TRUE
+
+if (testing){
+  # Convert all special characters to "normal" characters if running tests,
+  # because the sorting with special characters is system dependent.
+  
+  conv_list1 <- list("æ", "ø", "å", "Æ",  "Ø", "Å", '-', "St. ")
+  conv_list2 <- list("ae","o", "a", "AE", "O", "å", "_", "St ")
+  
+  for (i in 1:length(conv_list1)){
+    
+    area <- gsub(conv_list1[i], conv_list2[i], area)
+  }
+}
 
 # All data
 themes <- data.frame(tbl$themes) %>% tibble::as_data_frame()
-
-# Names, highest level
-level1 <- themes$name
 
 # Test that number of highest level is equal the length of themes$indicators
 if (length(themes$name) != length(themes$indicators)){
   stop("Something fishy in your json file. ")
 }
 
-all_data <- data.frame()
-
-test <- themes$indicators[2]
-
-test2 <- data.frame(test)
-
-test2$date
-
-print()
 
 next_level <- data.frame(themes$indicators[1])
-
-next_level$id
-next_level$values
-
-themes$name
-next_level$values[1]
-
-next_level$associates
+rates <- data.frame(next_level$values)  %>% tibble::as_data_frame()
+ref_rates <- data.frame(next_level$comparisonValues)  %>% tibble::as_data_frame()
 
 
+
+# Define an empty data frame
 all_data <- data.frame()
 for (i in 1:length(themes$indicators)){
+  # Names for first level
   level1 <- themes$name[i]
+  prev_level3 <- "qwerty" # To check if level3 is equal to previous level3
+  k = 0
   next_level <- data.frame(themes$indicators[i])
   rates <- data.frame(next_level$values)  %>% tibble::as_data_frame()
+  ref_rates <- data.frame(next_level$comparisonValues)  %>% tibble::as_data_frame()
   for (j in 1:length(next_level)){
     if (!is.na(next_level$id[j])){
+      # Names for the second level
       level2 <- next_level$name[j]
-      level3 <- next_level$date[j]
+      level3 <- NULL
+      # Names for the third level, if it exists
+      level3 <- try(next_level$date[j])
+      # ID for level 2 (not unique with three levels)
       selection_id <- next_level$id[j]
-      combined <- data.frame(bo$name, level1, level2, level3, selection_id, rates[j])
-
-      colnames(combined)[1] <- "bo"
-      colnames(combined)[2] <- "level1"
-      colnames(combined)[3] <- "level2"
-      colnames(combined)[4] <- "level3"
-      colnames(combined)[5] <- "id"
-      colnames(combined)[6] <- "rate"
-
-      all_data <- rbind(all_data, combined)
+      if (is.null(level3)){
+        # Only for two-level atlases
+        combined <- data.frame(area, level1, level2, selection_id, rates[j]) 
+        colnames(combined) <- c("area", "level1", "level2", "id", "rate")
+        ref_combined <- data.frame(ref_area, level1, level2, selection_id, ref_rates[j]) 
+        colnames(ref_combined) <- c("area", "level1", "level2", "id", "rate")
+        
+      } else { # Only for three level atlases
+        if (level3 != prev_level3){ # If level3 is not equal to previous level3
+          k = k + 1
+          id2 <- paste0(selection_id, "j", k)
+        }
+        combined <- data.frame(area, level1, level2, level3, id2, rates[j]) 
+        colnames(combined) <- c("area", "level1", "level2", "level3", "id", "rate")
+        ref_combined <- data.frame(ref_area, level1, level2, level3, id2, ref_rates[j]) 
+        colnames(ref_combined) <- c("area", "level1", "level2", "level3", "id", "rate")
+        prev_level3 <- level3
+      }
+      all_data <- rbind(all_data, combined, ref_combined)
     }
-  }
+  } 
 }
-
 print(all_data)
 
