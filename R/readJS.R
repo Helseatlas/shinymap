e40e0ab912067e08ee6c78ff8df3f9d9ab839fc4
Arnfinn <arnfinn.steindal@gmail.com>
2018-09-11 10:52:10 +0200

    Fixed bug in unique identifier, and more doc

--- R/readJS.R
+++ R/readJS.R
@@ -1,4 +1,8 @@
 #' Read json-data from IA
+#' 
+#' All the data used to produce an Instant Atlas is stored in
+#' a json file. This function will read such a file and put the
+#' data into a data frame.  
 #'
 #' @param json_file The json file used by IA
 #' @param testing Will convert special characters to non-special characters if set to TRUE
@@ -16,10 +20,10 @@ readIAjson <- function(json_file = NULL, testing = FALSE){
   # make it a tibble data fram
   tbl <- tibble::as_data_frame(json_data$geographies)
   
-  # Names of areas are located in json_data$geographies$features
+  # Names of areas are located in json_data$geographies$features$name
   area <- data.frame(tbl$features)$name
   
-  # Name of reference is located in json_data$geographies$comparisonFeatures
+  # Name of reference areas are located in json_data$geographies$comparisonFeatures$name
   ref_area <- data.frame(tbl$comparisonFeatures)$name
   
   if (testing){
@@ -35,22 +39,27 @@ readIAjson <- function(json_file = NULL, testing = FALSE){
     }
   }
   
-  # All data
+  # The rest of the data is located in json_data$geographies$themes
   themes <- data.frame(tbl$themes) %>% tibble::as_data_frame()
   
-  # Test that number of highest level is equal the length of themes$indicators
+  # Test that number of highest level names (json_data$geographies$themes$name) 
+  # is equal the length of the data (json_data$geographies$themes$indicators)
   if (length(themes$name) != length(themes$indicators)){
     stop("Something fishy in your json file. ")
   }
   
-  # Define an empty data frame
+  # Define an empty data frame, so we can add data to this frame in the loop.
   all_data <- data.frame()
-  for (i in 1:length(themes$indicators)){
+  
+  # Loop over level one
+  for (i in 1:length(themes$name)){
+
     # Names for first level
     level1 <- themes$name[i]
-    prev_level3 <- "qwerty" # To check if level3 is equal to previous level3
-    k = 0
+    
+    # Evereything else is stored in next_level
     next_level <- data.frame(themes$indicators[i])
+    
     # Rates to be plotted
     rates <- data.frame(next_level$values)  %>% tibble::as_data_frame()
     # Rates for Norway etc
@@ -61,8 +70,23 @@ readIAjson <- function(json_file = NULL, testing = FALSE){
     # Extract the numeraters and denominators
     extra <- data.frame(next_level$associates)
     
+    # Dummy definition, to later check if level3 is equal to previous level3
+    prev_level3 <- "qwerty"
+    
+    k = 0
+    
     for (j in 1:length(next_level)){
+      # Level 2
+      
       if (!is.na(next_level$id[j])){
+        
+        # Names for the second level
+        level2 <- next_level$name[j]
+        level3 <- NULL
+        # Names for the third level, if it exists
+        level3 <- try(next_level$date[j])
+        # ID for level 2 (not unique with three levels)
+        selection_id <- next_level$id[j]
         
         # numeraters and denominaters stored in extra$values.1 etc.
         k <- j - 1
@@ -78,23 +102,20 @@ readIAjson <- function(json_file = NULL, testing = FALSE){
         ref_numerater <-  data.frame(extra[, paste0("comparisonValues",post)][2])
         ref_denominator <- data.frame(extra[, paste0("comparisonValues",post)][1])
         
-        # Names for the second level
-        level2 <- next_level$name[j]
-        level3 <- NULL
-        # Names for the third level, if it exists
-        level3 <- try(next_level$date[j])
-        # ID for level 2 (not unique with three levels)
-        selection_id <- next_level$id[j]
         combined <- data.frame(area, level1, level2)
         colnames(combined) <- c("area", "level1", "level2")
         ref_combined <- data.frame(ref_area, level1, level2)
         colnames(ref_combined) <- c("area", "level1", "level2")
         
+        # Extract metatext, if present
         properties <- NULL
         properties <- try(next_level$properties)
         metatext <- NULL
         if (!is.null(properties)){
+          # Metatext can either be stored in next_level$properties[l]$value[n] or in
+          # next_level$properties$value[n], next_level$properties$value.1[n] etc.
           if (length(properties) > 1){
+            # If metatext is stored in next_level$properties[l]$value[n]
             for (l in 1:length(properties)){
               df_properties <- data.frame(properties[l])
               for (n in 1:length(df_properties$value)){
@@ -104,6 +125,7 @@ readIAjson <- function(json_file = NULL, testing = FALSE){
               } 
             }
           } else {
+            # If metatext is stored in next_level$properties$value[n], next_level$properties$value.1[n] etc.
             df_properties <- data.frame(properties)
             for (l in 1:(length(df_properties)/2)){
               m = l - 1
@@ -131,14 +153,15 @@ readIAjson <- function(json_file = NULL, testing = FALSE){
           ref_combined["id"] <- selection_id
           
         } else {
+          # A new unique identifier has to be defined if it is a three-level atlas
           if (level3 != prev_level3){ # If level3 is not equal to previous level3
             k = k + 1
             id2 <- paste0(selection_id, "j", k)
           }
           combined["level3"] <- level3 
           ref_combined["level3"] <- level3 
-          combined["id"] <- selection_id
-          ref_combined["id"] <- selection_id
+          combined["id"] <- id2
+          ref_combined["id"] <- id2
           prev_level3 <- level3
           
         }
--- tests/debugging/development.R
+++ tests/debugging/development.R
@@ -100,6 +100,7 @@ testing <- FALSE
 
 # COPY OF readJS.R
 
+
 # Read the json file
 # NOTE: The js-file HAS to be converted from UTF-8 BOM to UTF-8 (in notepad++) before this will work!
 json_data <- jsonlite::fromJSON(json_file)
@@ -107,10 +108,10 @@ json_data <- jsonlite::fromJSON(json_file)
 # make it a tibble data fram
 tbl <- tibble::as_data_frame(json_data$geographies)
 
-# Names of areas are located in json_data$geographies$features
+# Names of areas are located in json_data$geographies$features$name
 area <- data.frame(tbl$features)$name
 
-# Name of reference is located in json_data$geographies$comparisonFeatures
+# Name of reference areas are located in json_data$geographies$comparisonFeatures$name
 ref_area <- data.frame(tbl$comparisonFeatures)$name
 
 if (testing){
@@ -126,22 +127,27 @@ if (testing){
   }
 }
 
-# All data
+# The rest of the data is located in json_data$geographies$themes
 themes <- data.frame(tbl$themes) %>% tibble::as_data_frame()
 
-# Test that number of highest level is equal the length of themes$indicators
+# Test that number of highest level names (json_data$geographies$themes$name) 
+# is equal the length of the data (json_data$geographies$themes$indicators)
 if (length(themes$name) != length(themes$indicators)){
   stop("Something fishy in your json file. ")
 }
 
-# Define an empty data frame
+# Define an empty data frame, so we can add data to this frame in the loop.
 all_data <- data.frame()
-for (i in 1:length(themes$indicators)){
+
+# Loop over level one
+for (i in 1:length(themes$name)){
+  
   # Names for first level
   level1 <- themes$name[i]
-  prev_level3 <- "qwerty" # To check if level3 is equal to previous level3
-  k = 0
+  
+  # Evereything else is stored in next_level
   next_level <- data.frame(themes$indicators[i])
+  
   # Rates to be plotted
   rates <- data.frame(next_level$values)  %>% tibble::as_data_frame()
   # Rates for Norway etc
@@ -152,8 +158,23 @@ for (i in 1:length(themes$indicators)){
   # Extract the numeraters and denominators
   extra <- data.frame(next_level$associates)
   
+  # Dummy definition, to later check if level3 is equal to previous level3
+  prev_level3 <- "qwerty"
+  
+  k = 0
+  
   for (j in 1:length(next_level)){
+    # Level 2
+    
     if (!is.na(next_level$id[j])){
+      
+      # Names for the second level
+      level2 <- next_level$name[j]
+      level3 <- NULL
+      # Names for the third level, if it exists
+      level3 <- try(next_level$date[j])
+      # ID for level 2 (not unique with three levels)
+      selection_id <- next_level$id[j]
       
       # numeraters and denominaters stored in extra$values.1 etc.
       k <- j - 1
@@ -169,23 +190,20 @@ for (i in 1:length(themes$indicators)){
       ref_numerater <-  data.frame(extra[, paste0("comparisonValues",post)][2])
       ref_denominator <- data.frame(extra[, paste0("comparisonValues",post)][1])
       
-      # Names for the second level
-      level2 <- next_level$name[j]
-      level3 <- NULL
-      # Names for the third level, if it exists
-      level3 <- try(next_level$date[j])
-      # ID for level 2 (not unique with three levels)
-      selection_id <- next_level$id[j]
       combined <- data.frame(area, level1, level2)
       colnames(combined) <- c("area", "level1", "level2")
       ref_combined <- data.frame(ref_area, level1, level2)
       colnames(ref_combined) <- c("area", "level1", "level2")
       
+      # Extract metatext, if present
       properties <- NULL
       properties <- try(next_level$properties)
       metatext <- NULL
       if (!is.null(properties)){
+        # Metatext can either be stored in next_level$properties[l]$value[n] or in
+        # next_level$properties$value[n], next_level$properties$value.1[n] etc.
         if (length(properties) > 1){
+          # If metatext is stored in next_level$properties[l]$value[n]
           for (l in 1:length(properties)){
             df_properties <- data.frame(properties[l])
             for (n in 1:length(df_properties$value)){
@@ -195,6 +213,7 @@ for (i in 1:length(themes$indicators)){
             } 
           }
         } else {
+          # If metatext is stored in next_level$properties$value[n], next_level$properties$value.1[n] etc.
           df_properties <- data.frame(properties)
           for (l in 1:(length(df_properties)/2)){
             m = l - 1
@@ -222,14 +241,15 @@ for (i in 1:length(themes$indicators)){
         ref_combined["id"] <- selection_id
         
       } else {
+        # A new unique identifier has to be defined if it is a three-level atlas
         if (level3 != prev_level3){ # If level3 is not equal to previous level3
           k = k + 1
           id2 <- paste0(selection_id, "j", k)
         }
         combined["level3"] <- level3 
         ref_combined["level3"] <- level3 
-        combined["id"] <- selection_id
-        ref_combined["id"] <- selection_id
+        combined["id"] <- id2
+        ref_combined["id"] <- id2
         prev_level3 <- level3
         
       }
