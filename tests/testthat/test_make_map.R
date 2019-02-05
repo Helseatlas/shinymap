981da4ccaa771ebfaca8b57f69e32d6af76ce662
arnfinn <arnfinn.steindal@gmail.com>
2019-02-05 12:57:53 +0100

    rm ggplotmap

--- tests/testthat/test_make_map.R
+++ tests/testthat/test_make_map.R
@@ -3,7 +3,3 @@ context("make_map")
 test_that("leaflet map is made", {
   expect_equal_to_reference(makeLeafletmap(), "data/leaflet.rds")
 })
-
-test_that("ggplot2 map is made", {
-  expect_equal_to_reference(plotggmap(mapfile = ), "data/ggmap.rds")
-})
