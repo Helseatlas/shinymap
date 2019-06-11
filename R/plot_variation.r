2eaa9280437f8cbfb5f826fbfce7e1423e8bdf9f
arnfinn <arnfinn.steindal@gmail.com>
2019-06-11 15:52:10 +0200

    New function skde_colors
    Is used by plot_variation function

--- R/plot_variation.r
+++ R/plot_variation.r
@@ -5,30 +5,29 @@
 #'   "histogram"
 #' @param xlab Label on x-axis
 #' @param ylab Label on y-axis
+#' @param num_groups Number of natural break groups
 #'
 #' @export
-plot_variation <- function(input_data = NULL, xlab = "Opptaksomr", ylab = "Rate", type = "histogram") {
+plot_variation <- function(input_data = NULL, xlab = "Opptaksomr", ylab = "Rate", type = "histogram", num_groups = 4) {
 
-  if (is.null(input_data)) {return(NULL)}
+  # Avoid errors if not everything is updated on the server side
+  if (is.null(input_data) || length(input_data$value) == 0) {
+    return(NULL)
+  }
 
-  # barplot
   if (type == "histogram") {
-    input_data$area_name <- factor(input_data$area_name, levels = input_data$area_name[order(input_data$rate)])
-    
-    library(classInt)
-    rate_num<-as.numeric(input_data$rate)
-    #print(rate_num)
-    rate_num[is.na(rate_num)]<-0
-    #print(rate_num)
-    interv<-c(classIntervals(rate_num , style="jenks"))
-    brks<-unique(interv$brks)
-    print(brks)
-    input_data$brks<-cut(rate_num, breaks=brks)
-    
+    # barplot
+    input_data$area_name <- factor(input_data$area_name, levels = input_data$area_name[order(input_data$value)])
+    rate_num <- as.numeric(input_data$value)
+    rate_num[is.na(rate_num)] <- 0
+    interv <- c(classInt::classIntervals(var = rate_num, n = num_groups, style = "jenks"))
+    brks <- unique(interv$brks)
+    input_data$brks <- cut(rate_num, breaks = brks, include.lowest = TRUE)
+
     ggplot2::ggplot(data = input_data,
       ggplot2::aes(x = get("area_name"), y = get("value"), fill = brks)) +
       ggplot2::geom_bar(stat = "identity") +
-      ggplot2::scale_fill_manual(values=c("#0099CC", "#33CCFF","#0033CC", "#66CCFF","#6699FF","#3366FF")) +
+      ggplot2::scale_fill_manual(values = shinymap::skde_colors(num = num_groups)) +
       ggplot2::labs(x = xlab, y = ylab) +
       ggplot2::coord_flip() +
       ggthemes::theme_tufte()
