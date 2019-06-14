420096f48d34493f2e383b8f7f2283058e8e02bf
arnfinn <arnfinn.steindal@gmail.com>
2019-06-14 10:38:20 +0200

    The package "maps" renamed to "kart"
    - Moved utm33_to_leaflet function to kart package
    
    Moved utm33_to_leaflet to kart package

--- R/shp2geojson.R
+++ R/shp2geojson.R
@@ -33,21 +33,3 @@ shp2geojson <- function(shapefile = "eldre",
 
   return(geojson_map)
 }
-
-#' Convert map from UTM 33 projection to epsg:4326 projection
-#'
-#' @param map map to be converted
-#'
-#' @return Converted map
-#'
-#' @export
-#'
-utm33_to_leaflet <- function(map) {
-  # utm33 is "epsg:32633"
-  # leaflet is "epsg:4326"
-
-  suppressWarnings(sp::proj4string(map) <- sp::CRS("+init=epsg:32633"))
-  new <- sp::CRS("+init=epsg:4326") # WGS 84
-  map_transformed <- sp::spTransform(map, new)
-  return(map_transformed)
-}
