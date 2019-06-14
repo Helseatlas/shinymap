c38a1cde7d33ee65c199d92ff13bebd497546a13
arnfinn <arnfinn.steindal@gmail.com>
2019-06-14 12:35:10 +0200

    rm utm33_to_leaflet test

--- tests/testthat/test_shp2geojson.R
+++ tests/testthat/test_shp2geojson.R
@@ -46,9 +46,3 @@ test_that("shp2geojson is OK", {
   )
   file.remove("tmp.geojson")
 })
-
-test_that("utm33_to_leaflet is OK", {
-  # Unit test that map can be converted from utm33 to leaflet projection
-  map <- geojsonio::geojson_read("data/maps/test.geojson", what = "sp")
-  expect_equal_to_reference(utm33_to_leaflet(map), "data/utm33_to_leaflet.rds", tolerance = 1e-4)
-})
