f643a94ebfa5352117dac5aa63a102521d3c2d59
arnfinn <arnfinn.steindal@gmail.com>
2019-06-19 14:06:21 +0200

    fix lintr; rm ref data

--- tests/testthat/test_plot_variation.R
+++ tests/testthat/test_plot_variation.R
@@ -3,15 +3,5 @@ context("plot_variation")
 test_that("plot_variation is OK", {
   expect_null(plot_variation())
 
-#  data <- readRDS("data/start_plot.rds")
-#  expect_equal_to_reference(plot_variation(input_data = data),
-#                            "data/plot_variation1.rds")
-
-#  expect_equal_to_reference(plot_variation(input_data = data, num_groups = 4),
-#                            "data/plot_variation1.rds")
-
-#  expect_equal_to_reference(plot_variation(input_data = data, num_groups = 11),
-#                            "data/plot_variation2.rds")
-
   expect_null(plot_variation(input_data = data, type = "tmp"))
 })
