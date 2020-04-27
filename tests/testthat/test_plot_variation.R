d9d6f11f0e187734d2e68d53ee5cfb6cf5d0c21e
arnfinn <arnfinn.steindal@gmail.com>
2020-04-27 14:29:58 +0200

    test make_map

--- tests/testthat/test_plot_variation.R
+++ tests/testthat/test_plot_variation.R
@@ -7,7 +7,7 @@ test_that("plot_variation is OK", {
   expect_null(plot_variation(input_data = data, type = "tmp"))
 
   test_plot <- plot_variation(input_data = data)
-  testthat::expect_equal_to_reference(test_plot$data, "data/plot_variation_data.rds")
-  testthat::expect_equal_to_reference(test_plot$labels, "data/plot_variation_labels.rds")
-  testthat::expect_equal_to_reference(test_plot$mapping, "data/plot_variation_mapping.rds")
+  expect_equal_to_reference(test_plot$data, "data/plot_variation_data.rds")
+  expect_equal_to_reference(test_plot$labels, "data/plot_variation_labels.rds")
+  expect_equal_to_reference(test_plot$mapping, "data/plot_variation_mapping.rds")
 })
