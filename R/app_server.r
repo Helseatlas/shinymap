9a0ff98d86cf75eb3c00aa7707839ab7b18585b4
arnfinn <arnfinn.steindal@gmail.com>
2020-05-07 16:09:11 +0200

    mv selectize into module
    WIP. Not working

--- R/app_server.r
+++ R/app_server.r
@@ -10,6 +10,12 @@ app_server <- function(input, output, session) {
     healthatlas_data <- get_data() # nolint
     config <- get_config() # nolint
 
+    select_server("test1",
+                  language = reactive(input$language),
+                  healthatlas_data = healthatlas_data,
+                  config = config
+                  )
+
     if (!exists("git_hash")) {
       git_hash <- NULL
     }
@@ -36,30 +42,6 @@ app_server <- function(input, output, session) {
           ),
           selected = "nb"
         )
-    })
-
-    output$pick_atlas <- shiny::renderUI({
-      shiny::req(input$language)
-      if (!is.data.frame(healthatlas_data)) {
-        mytitle <- c()
-        atlasnames <- names(healthatlas_data)
-        for (i in atlasnames) {
-          if (input$language == "nb") {
-            mytitle <- c(mytitle, paste(healthatlas_data[[i]]$title_no, healthatlas_data[[i]]$year, sep = " "))
-          } else if (input$language == "en") {
-            mytitle <- c(mytitle, paste(healthatlas_data[[i]]$title_en, healthatlas_data[[i]]$year, sep = " "))
-          }
-        }
-        names(atlasnames) <- mytitle
-
-        shiny::selectInput(
-          selectize = FALSE,
-          inputId = "atlas",
-          label = config$menus$atlas[[input$language]],
-          choices = atlasnames,
-          selected = atlasnames[1]
-        )
-      }
     })
 
     atlas_data <- shiny::reactive({
@@ -95,80 +77,6 @@ app_server <- function(input, output, session) {
       }
     })
 
-    output$pick_level1 <- shiny::renderUI({
-      shiny::req(input$language)
-      # Possible values for level 1
-      pickable_level1 <- unique(factor(atlas_data()$level1_name))
-      # The selector
-      shiny::selectInput(
-        selectize = FALSE,
-        inputId = "menu_level1",
-        label = config$menus$level[[input$language]],
-        choices = pickable_level1,
-        selected = pickable_level1[1]
-      )
-    })
-
-    output$pick_level2 <- shiny::renderUI({
-      if ("level2_name" %in% colnames(atlas_data())) {
-        if (is.null(input$menu_level1)) {
-          return(NULL)
-        }
-        # Filter out data according to what is choosen for level 1
-        tmpdata <- dplyr::filter(atlas_data(), atlas_data()$level1_name == input$menu_level1)
-        # Possible values for level 2
-        pickable_level2 <- unique(factor(tmpdata$level2_name))
-        if (suppressWarnings(all(!is.na(as.numeric(as.character(pickable_level2)))))) {
-          # If all the pickable values are integers then it is probably year
-          years <- as.numeric(as.character(pickable_level2))
-          if (is.null(years) | length(years) == 0) {
-            return(NULL)
-          }
-          shiny::tags$div(class = "year-slider", shiny::sliderInput(
-            inputId = "menu_level2",
-            label = config$menus$year[[input$language]],
-            min = min(years),
-            max = max(years),
-            step = 1,
-            sep = "",
-            ticks = TRUE,
-            animate = TRUE,
-            value = max(years)
-          ))
-        } else {
-          # The selector
-          shiny::selectInput(
-            selectize = FALSE,
-            inputId = "menu_level2",
-            label = config$menus$level[[input$language]],
-            choices = pickable_level2,
-            selected = pickable_level2[1]
-          )
-        }
-      }
-    })
-
-    output$pick_level3 <- shiny::renderUI({
-      if ("level3_name" %in% colnames(atlas_data())) {
-        if (is.null(input$menu_level2)) {
-          return(NULL)
-        }
-        # Filter out data according to what is choosen for level 1
-        tmpdata1 <- dplyr::filter(atlas_data(), atlas_data()$level1_name == input$menu_level1)
-        # Filter out data according to what is choosen for level 2
-        tmpdata2 <- dplyr::filter(tmpdata1, tmpdata1$level2_name == input$menu_level2)
-        # Possible values for level 3
-        pickable_level3 <- unique(factor(tmpdata2$level3_name))
-        # The selector
-        shiny::selectInput(
-          selectize = FALSE,
-          inputId = "menu_level3",
-          label = config$menus$level[[input$language]],
-          choices = pickable_level3,
-          selected = pickable_level3[1]
-        )
-      }
-    })
 
     output$git_version <- shiny::renderUI({
       if (!is.null(git_hash)) {
