58a209ad9a84266226384e6a973f7c2184ca537d
arnfinn <arnfinn.steindal@gmail.com>
2020-05-11 11:28:20 +0200

    reindent code as in master

--- R/app_ui.r
+++ R/app_ui.r
@@ -9,29 +9,29 @@ app_ui <- function() {
                      shiny::tags$link(rel = "icon", type = "image/png", href = "www/hn.png"),
                      shiny::tags$title("Helseatlas"),
                      shiny::tags$link(rel = "stylesheet", type = "text/css", href = "www/custom.css")
-                   )),
-                   shiny::fluidRow(
-                     shiny::column(8,
-                                   shiny::uiOutput("title")
-                     ),
-                     shiny::column(1, offset = 3,
-                                   shiny::uiOutput("pick_language")
-                     )
-                   ),
-                   shiny::fluidRow(
-                     shiny::column(3,
-                                   shiny::uiOutput("pick_atlas"),
-                                   shiny::uiOutput("pick_level1"),
-                                   shiny::uiOutput("pick_level2"),
-                                   shiny::uiOutput("pick_level3"),
-                                   shiny::uiOutput("app_info"),
-                                   shiny::uiOutput("git_version")
-                     ),
-                     shiny::column(9,
-                                   shiny::tabsetPanel(
-                                     tabPanel(shiny::icon("globe"),       tab_ui1("plots")),
-                                     tabPanel(shiny::icon("chart-bar"),   tab_ui2("plots")),
-                                     tabPanel(shiny::icon("table"),       tab_ui3("plots"))
-                                   ))
-                   ))
+                  )),
+                  shiny::fluidRow(
+                    shiny::column(8,
+                                  shiny::uiOutput("title")
+                    ),
+                    shiny::column(1, offset = 3,
+                                  shiny::uiOutput("pick_language")
+                    )
+                  ),
+                  shiny::fluidRow(
+                    shiny::column(3,
+                                  shiny::uiOutput("pick_atlas"),
+                                  shiny::uiOutput("pick_level1"),
+                                  shiny::uiOutput("pick_level2"),
+                                  shiny::uiOutput("pick_level3"),
+                                  shiny::uiOutput("app_info"),
+                                  shiny::uiOutput("git_version")
+                    ),
+                    shiny::column(9,
+                                  shiny::tabsetPanel(
+                                    tabPanel(shiny::icon("globe"),       tab_ui1("plots")),
+                                    tabPanel(shiny::icon("chart-bar"),   tab_ui2("plots")),
+                                    tabPanel(shiny::icon("table"),       tab_ui3("plots"))
+                                  ))
+                  ))
 }
