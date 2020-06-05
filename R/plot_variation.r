750904178d1134eedc21625c387a7827252c9326
Janice <janice.the@gmail.com>
2020-06-05 10:38:20 +0200

    paste0 and ": "

--- R/plot_variation.r
+++ R/plot_variation.r
@@ -34,10 +34,10 @@ plot_variation <- function(input_data = NULL, xlab = "Area", ylab = "Rate", type
     the_plot <-
       ggplot2::ggplot(data = input_data,
       ggplot2::aes(x = get("area_name"), y = get("value"), fill = get("brks"),
-                   text = paste("<b>", get("area_name"), "</b><br>",
-                                get("type"), ":", format(get("value"),
+                   text = paste0("<b>", get("area_name"), "</b><br>",
+                                get("type"), ": ", format(get("value"),
                                 big.mark = big_mark, decimal.mark = decimal_mark, digits = 2), "<br>",
-                                get("numerator_name"), ":", format(get("numerator"),
+                                get("numerator_name"), ": ", format(get("numerator"),
                                 big.mark = big_mark, decimal.mark = decimal_mark, digits = 2)))) +
 
       ggplot2::geom_bar(stat = "identity") +
